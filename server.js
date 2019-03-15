d8f6c2e84b7c7cc8824e36f04ca195f243e275d5
t0nxx <mahmoudko1500@hotmail.com>
2019-03-15 23:55:49 +0200

    add morgan logger

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app  = express();
 const cors = require('cors');
+const morgan = require('morgan');
 const {AdminRouter} = require('./src/api/v2/routes/admin');
 const {CompanyRouter} = require('./src/api/v2/routes/company');
 const {OfferRouter} = require('./src/api/v2/routes/offer');
@@ -10,9 +11,12 @@ const {AuthRouter} = require('./src/api/v2/routes/auth');
 const {AboutUsRouter} = require('./src/api/v2/routes/aboutUs');
 const {MessagesRouter} = require('./src/api/v2/routes/message');
 const {NotificationRouter} = require('./src/api/v2/routes/notification');
+const fs = require('fs');
+const accessLogStream = fs.createWriteStream(__dirname + '/logs.log', {flags: 'a'});
 require('dotenv').config();
 app.use(express.json());
 app.use(cors());
+app.use(morgan('combined',{skip: function (req, res) { return res.statusCode < 400 },stream : accessLogStream}));
 app.use('/admins' , AdminRouter);
 app.use('/companies' , CompanyRouter);
 app.use('/offers' , OfferRouter);
@@ -28,8 +32,11 @@ app.get('/' , (req,res) => {
 app.get('/testoffer' , (req,res) => {
     res.sendFile(__dirname+'/testup.html');
 });
+app.get('/logs' , (req,res) => {
+    res.sendFile(__dirname+'/logs.log');
+});
 app.use('*' , (req,res) => {
-    res.status(404).send("no route was found ");
+    res.status(404).send("404 Error Page Not Found");
 });
 
 exports.app = app ;
