08cd750c2ebceb97d269573ab1deb49083af5c1c
t0nxx <mahmoudko1500@hotmail.com>
2019-03-23 15:13:01 +0200

    stop sync in production

--- src/api/v2/models/offer.js
+++ src/api/v2/models/offer.js
@@ -75,16 +75,16 @@ offerSchema.plugin(mongoosastic,{
   });
 const Offer = model ('Offer' , offerSchema);
 
-let stream = Offer.synchronize()
-, count = 0;
+// let stream = Offer.synchronize()
+// , count = 0;
 
-stream.on('data', function(err, doc){
-count++;
-});
-stream.on('close', function(){
-console.log('indexed ' + count + ' documents!');
-});
-stream.on('error', function(err){
-console.log(err);
-});
+// stream.on('data', function(err, doc){
+// count++;
+// });
+// stream.on('close', function(){
+// console.log('indexed ' + count + ' documents!');
+// });
+// stream.on('error', function(err){
+// console.log(err);
+// });
 exports.Offer=Offer;
