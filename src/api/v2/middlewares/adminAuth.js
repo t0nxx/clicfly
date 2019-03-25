ad88a1788b20d8ea3d807937d5e6990c2de702bd
t0nxx <mahmoudko1500@hotmail.com>
2019-03-25 23:05:32 +0200

    add 403,404 at auth middleware

--- src/api/v2/middlewares/adminAuth.js
+++ src/api/v2/middlewares/adminAuth.js
@@ -1,11 +1,12 @@
 const jwt = require('jsonwebtoken');
 const {Admin} = require('../models/admin');
 const crypto = require('crypto-js');
+const path = require('path');
 require('dotenv').config();
 const AdminAuth = async(req,res,next)=>{
     if (!process.env.EnableAuth) return next(); // disable auth in dev
     const token = req.header("Authorization");
-    if (!token) {return res.status(403).sendFile(__dirname+'/403.html');} 
+    if (!token) {return res.status(403).sendFile(path.join(__dirname , '../../../../403.html'));} 
     else if (token === process.env.MASTER_SECRET){ next();}  /// master key for all
     else try {
             const decode = await jwt.verify(token,process.env.JWT_SECRET_ADMIN);
