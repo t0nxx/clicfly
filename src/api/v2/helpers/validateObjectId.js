1cae233a51eca7d17eddeebb3c0871b73de8b0d2
t0nxx <mahmoudko1500@hotmail.com>
2019-03-25 23:32:04 +0200

    valid id custom msg 404

--- src/api/v2/helpers/validateObjectId.js
+++ src/api/v2/helpers/validateObjectId.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const ObjectId = mongoose.Types.ObjectId;
 
 const validIdObject = (id)=>{
-    if (!ObjectId.isValid(id.toString())) throw new Error ("Invalid id");
+    if (!ObjectId.isValid(id.toString())) throw new Error ("404 Not Found");
 }
 
 exports.validIdObject=validIdObject;
