2a1cb09d44bcb8008c66baeced75f63964f55dae
t0nxx <mahmoudko1500@hotmail.com>
2019-04-21 00:49:11 +0200

    run traveloffer mail api

--- src/api/v2/helpers/sendMail.js
+++ src/api/v2/helpers/sendMail.js
@@ -2,12 +2,12 @@ const api_key = '9865d400e70784476d638839493deaee-c8e745ec-4aefb90e';
 const domain = 'test.1millionofcodes.com';
 const api_key2 = 'e3efb00fff61f48e481faf223cdbf13c-2416cf28-14c5b44e';
 const domain2 = 'traveloffersnhotels.com';
-const mailgun = require('mailgun-js')({apiKey: api_key, domain: domain});
+const mailgun = require('mailgun-js')({apiKey: api_key2, domain: domain2});
  
 function sendMail(mail,resetCode){
     const data = {
-        // from: "TravelOffers no-replay@traveloffersnhotels.com",
-        from: "Toni toni@test.1millionofcodes.com",
+        from: "TravelOffers no-replay@traveloffersnhotels.com",
+        // from: "Toni toni@test.1millionofcodes.com",
         to: `${mail}`,
         subject: 'Reset Password',
         text: `Your code is ${resetCode} 
@@ -18,5 +18,5 @@ function sendMail(mail,resetCode){
         console.log(body);
       });
 }
-//sendMail('abdelrahman.ui@gmail.com');
+//sendMail('hhaker95@gamil.com' , 2566);
 exports.sendMail = sendMail ;
